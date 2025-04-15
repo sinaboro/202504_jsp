0241c25bbecd1219fb5b4fbb9be62ed47071be86
sinaboro <rlaeocjf@gmail.com>
2025-04-15 11:14:19 +0900

    .......

---
+++ web-study-09/src/main/java/com/saeyan/controller/LoginServlet.java
@@ -0,0 +1,38 @@
+package com.saeyan.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.saeyan.dao.MemberDAO;
+
+
+@WebServlet("/login.do")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher dis = request.getRequestDispatcher("member/login.jsp");
+		dis.forward(request, response);
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String userid = request.getParameter("userid");
+		String pwd = request.getParameter("pwd");
+		
+		//DB연결해서 userid, pwd 해당 자가 있는지 확인!
+		MemberDAO mDao = MemberDAO.getInstance();
+		mDao.userCheck(userid, pwd);
+	}
+
+}
--- web-study-09/src/main/java/com/saeyan/dao/MemberDAO.java
+++ web-study-09/src/main/java/com/saeyan/dao/MemberDAO.java
@@ -2,6 +2,8 @@ package com.saeyan.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class MemberDAO {
 	
@@ -27,5 +29,38 @@ public class MemberDAO {
 		//2. DB연결	    
 		
 		return DriverManager.getConnection(url,uid,pass);
+	}
+
+	//userid, pwd 전달받아서, DB랑 연동해서 데이타가 있는지 조회
+	public void userCheck(String userid, String pwd) {
+		
+		/*
+		 * 1  : userid, pwd 일치
+		 * 0  : userid 일치, pwd 불일치
+		 * -1 : userid 불일치 
+		 */
+		
+		int result = -1;
+		
+		String sql = "select pwd from member where userid = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if ( rs != null) rs.close();
+				if ( pstmt != null) pstmt.close();
+				if ( conn != null) conn.close();
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 }
